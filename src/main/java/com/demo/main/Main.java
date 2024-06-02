5706e9139ce0d7eac22ce883041e10432a2fc464
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-06-02 13:52:22 +0530

    Native SQL quieries

--- src/main/java/com/demo/main/Main.java
+++ src/main/java/com/demo/main/Main.java
@@ -1,9 +1,6 @@
 package com.demo.main;
 
 import com.demo.entity.User;
-
-import jakarta.persistence.Query;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,43 +8,46 @@ import org.hibernate.cfg.Configuration;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        // Create a Hibernate configuration object
-        Configuration configuration = new Configuration().configure();
+	public static void main(String[] args) {
+		// Create a Hibernate configuration object
+		Configuration configuration = new Configuration().configure();
 
-        // Create a session factory
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
+		// Create a session factory
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
 
-        // Open a session
-        Session session = sessionFactory.openSession();
+		// Open a session
+		Session session = sessionFactory.openSession();
 
-        // Begin a transaction
-        Transaction transaction = session.beginTransaction();
+		// Begin a transaction
+		Transaction transaction = session.beginTransaction();
 
-            // 1. Retrieve all users
-            System.out.println("Retrieving all users:");
-            Query queryRetrieveAllUsers = session.createQuery("SELECT u FROM User u", User.class);
-            List<User> allUsers = queryRetrieveAllUsers.getResultList();
-            for (User user : allUsers) {
-                System.out.println("User ID: " + user.getId() + ", Name: " + user.getName() + ", Email: " + user.getEmail());
-            }
+		// Execute Native SQL query to retrieve all users
+		List<User> allUsers = session.createNativeQuery("SELECT * FROM user", User.class).list();
+		for (User user : allUsers) {
+			System.out
+					.println("User ID: " + user.getId() + ", Name: " + user.getName() + ", Email: " + user.getEmail());
+		}
 
-            // 2. Retrieve users based on specific criteria
-            System.out.println("\nRetrieving users with name 'John Doe':");
-            String jpqlRetrieveUsersByName = "SELECT u FROM User u WHERE u.name = :userName";
-            Query queryRetrieveUsersByName = session.createQuery(jpqlRetrieveUsersByName, User.class);
-            queryRetrieveUsersByName.setParameter("userName", "kiran");
-            List<User> usersByName = queryRetrieveUsersByName.getResultList();
-            for (User user : usersByName) {
-                System.out.println("User ID: " + user.getId() + ", Name: " + user.getName() + ", Email: " + user.getEmail());
-            }
+		// Execute Native SQL query to retrieve users with a specific name
+		List<User> usersWithNameJohn = session
+				.createNativeQuery("SELECT * FROM user WHERE name = 'Kiran'", User.class).list();
+		for (User user : usersWithNameJohn) {
+			System.out
+					.println("User ID: " + user.getId() + ", Name: " + user.getName() + ", Email: " + user.getEmail());
+		}
 
-            // Commit the transaction
-            transaction.commit();
-      
-            // Close session and session factory
-            session.close();
-            sessionFactory.close();
-        
-        }
+		// Execute Native SQL update query to update email of a user
+		int rowsUpdated = session.createNativeQuery("UPDATE user SET email = 'Kommukiran1226@example.com' WHERE id = 1")
+				.executeUpdate();
+		System.out.println("Rows updated: " + rowsUpdated);
+
+
+		// Commit the transaction
+		transaction.commit();
+
+		// Close session and session factory
+		session.close();
+		sessionFactory.close();
+
+	}
 }
