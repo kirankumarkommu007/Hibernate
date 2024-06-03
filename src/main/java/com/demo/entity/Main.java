39664d5f89832c81f28b41be0f8f3c91da68f420
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-06-03 14:43:49 +0530

    Updated

--- src/main/java/com/demo/entity/Main.java
+++ src/main/java/com/demo/entity/Main.java
@@ -13,24 +13,19 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		// Create a Hibernate configuration object, which reads the hibernate.cfg.xml
-		// file
+	
 		Configuration configuration = new Configuration().configure();
 
-		// Create a session factory based on the configuration
 		SessionFactory sessionFactory = configuration.buildSessionFactory();
 
-		// Open a session from the session factory
 		Session session = sessionFactory.openSession();
 
-		// Get the CriteriaBuilder instance from the session to construct the query
 		CriteriaBuilder cb = session.getCriteriaBuilder();
 
-		// Create a CriteriaQuery object for the Employee entity
-		CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+		CriteriaQuery<NewUser> cq = cb.createQuery(NewUser.class);
 
 		// Define the root of the query, which is the Employee entity
-		Root<Employee> root = cq.from(Employee.class);
+		Root<NewUser> root = cq.from(NewUser.class);
 
 		// Create a condition (predicate) to find employees with id = 1
 		Predicate predicate = cb.equal(root.get("id"), "1");
@@ -39,15 +34,13 @@ public class Main {
 		cq.select(root).where(predicate);
 
 		// Execute the query and retrieve the results as a list of Employee objects
-		List<Employee> result = session.createQuery(cq).getResultList();
+		List<NewUser> result = session.createQuery(cq).getResultList();
 
 		// Iterate over the results and print the fetched Employee details
-		for (Employee user : result) {
+		for (NewUser user : result) {
 			System.out.println("Fetched User: " + user.getName() + ", " + user.getEmail());
 		}
 
-		// Uncomment these lines if you want to close the session and session factory
-		// newSession.close();
-		// sessionFactory.close();
+		 sessionFactory.close();
 	}
 }
